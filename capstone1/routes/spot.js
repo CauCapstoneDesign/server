e0a9ae7824b1bcb6113f6f886d373c3487c15f4c
SouthColumn76 <nrkim01@gmail.com>
2019-10-29 02:36:09 +0900

    spotaaa

--- capstone1/routes/spot.js
+++ capstone1/routes/spot.js
@@ -21,7 +21,7 @@ app.post('/newspot', function (req, res) {
 
     var sql = 'INSERT INTO SpotInformation (location, authorID, grade) VALUES(?, ?, ?)';
     var params = [location, authorID, grade];
-    connection.query(sql2, params, function (err, rows, fields) {
+    connection.query(sql, params, function (err, rows, fields) {
         if (err) {
             console.log(err);
         } else {
