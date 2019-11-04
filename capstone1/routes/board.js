daf742b963608a529d270e66fbe1cc522cbb140f
Choi Changhwan <ccwhan6@naver.com>
2019-11-04 21:49:13 +0900

    asdfeee

---
+++ capstone1/routes/board.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var mysql = require('mysql');
+
+var app = express();
+app.use(express.json());
+
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    database: "starstarstar",
+    password: "Ironman3000!",
+    port: 3306
+});
+
+app.post('/newpost', function (req, res) {
+    console.log(req.body);
+    var title = req.body.title;
+    var content = req.body.content;
+    var author_id = req.body.author_id;
+    var photo_url = req.body.photo_url;
+    var done = "no";
+
+    var sql = 'INSERT INTO board ( title, content, author_id) VALUES( ?, ?, ?)';
+    var params = [title, content, author_id];
+    connection.query(sql, params, function (err, rows, fields) {
+        if (err) {
+            console.log(err);
+            res.json({
+                'status': "no"
+            });
+        } else {
+            const map1 = photo_url.map(x => {
+                return [rows.insertId];
+            })
+            sql = 'INSERT INTO board ( board_id, photo_url) VALUES( ?, ?, ?)'
+            connection.query(sql, map1, function (err, rows, fields) {
+                if (err) {
+                    res.json({
+                        'status': "no"
+                    })
+                } else {
+                    res.json({
+                        'status': "done"
+                    });
+                }
+            });
+        }
+    });
+})
+
+app.get('/getpost', function (req, res, next) {
+    connection.query('SELECT * from SpotInformation', function (err, rows, fields) {
+        if (!err)
+            res.send(rows);
+        else
+            console.log('Error while performings Query.', err);
+    });
+});
+
+module.exports = app;
--- capstone1/routes/spot.js
+++ capstone1/routes/spot.js
@@ -7,12 +7,39 @@ app.use(express.json());
 var connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
-    database: "sys",
+    database: "starstarstar",
     password: "Ironman3000!",
     port: 3306
 });
 
 app.post('/newspot', function (req, res) {
+    console.log(req.body);
+    var id = req.body.id;
+    var author_id = req.body.author_id;
+    var address = req.body.address;
+    var photo_url = req.body.photo_url;
+    var latitude = req.body.latitude;
+    var longitude = req.body.longitude;
+    var done = "no";
+
+    var sql = 'INSERT INTO spot (id, name, address, photo_url, author_id, latitude, longitude) VALUES(?, ?, ?)';
+    var params = [id, name, address, photo_url, author_id, latitude, longitude];
+    connection.query(sql, params, function (err, rows, fields) {
+        if (err) {
+            console.log(err);
+            res.json({
+                'status': "no"
+            });
+        } else {
+            console.log(rows.insertId);
+            res.json({
+                'status': "done"
+            });
+        }
+    });
+})
+
+app.post('/spotsearchbylocation', function (req, res) {
     console.log(req.body);
     var location = req.body.location;
     var authorID = req.body.authorID;
@@ -35,19 +62,15 @@ app.post('/newspot', function (req, res) {
             });
         }
     });
-
 })
 
-
 app.get('/getspot', function (req, res, next) {
-
     connection.query('SELECT * from SpotInformation', function (err, rows, fields) {
         if (!err)
             res.send(rows);
         else
             console.log('Error while performings Query.', err);
     });
-
 });
 
 module.exports = app;
